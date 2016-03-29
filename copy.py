e52dd6d211cd394788225e89cd440dd9ce062b38
Thomas Levine <_@thomaslevine.com>
2016-03-29 15:16:16 +0000

    verbose and updates

--- copy.py
+++ copy.py
@@ -1,8 +1,11 @@
 import os
+import logging
 
 import boto3
 
-def copy_from_s3(bucketname, localdir=None):
+logger = logging.getLogger(__name__)
+
+def copy_from_s3(bucketname, localdir=None, verbose=False):
     s3 = boto3.resource('s3')
     if not localdir:
         localdir = bucketname
@@ -10,5 +13,14 @@ def copy_from_s3(bucketname, localdir=None):
     for obj in bucket.objects.all():
         if obj.size > 0:
             destination = os.path.abspath(os.path.join(localdir, obj.key))
-            os.makedirs(os.path.dirname(destination), exist_ok=True)
-            bucket.download_file(obj.key, destination)
+            if os.path.exists(destination):
+                if verbose:
+                    logger.info('Already downloaded ' + obj.key)
+            else:
+                os.makedirs(os.path.dirname(destination), exist_ok=True)
+                bucket.download_file(obj.key, destination)
+                if verbose:
+                    logger.info('Downloaded ' + obj.key)
+        else:
+            if verbose:
+                logger.info('Skipped empty ' + obj.key)
